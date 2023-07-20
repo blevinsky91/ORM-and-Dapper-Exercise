18c25bd740d32400239b8128aa907ea96ea5fa20
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-20 15:43:47 -0400

    ex2

--- BestBuyBestPractices/BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/BestBuyBestPractices/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using System.Data;
 
@@ -10,4 +11,24 @@ var config = new ConfigurationBuilder()
 string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DapperProductRepository(conn);
+
+Console.WriteLine("What is the name of your new product?");
+var prodName = Console.ReadLine();
+
+Console.WriteLine("What is the price?");
+var prodPrice = double.Parse(Console.ReadLine());
+
+Console.WriteLine("What is the category ID?");
+var prodCat = int.Parse(Console.ReadLine());
+
+repo.CreateProduct(prodName, prodPrice, prodCat);
+
+var prodList = repo.GetAllProducts();
+
+foreach(var item in prodList)
+{
+    Console.WriteLine($"{item.ProductID} - {item.Name}");
+}
 
